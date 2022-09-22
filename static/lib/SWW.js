44aaf77f8d584b45ff94351bcf8019a291d41487
howlingunit <93931085+howlingunit@users.noreply.github.com>
2022-09-22 23:24:44 +0100

    home page V 0.3 implementation

--- static/lib/SWW.js
+++ static/lib/SWW.js
@@ -4,19 +4,17 @@ const sleep = (milliseconds) => {
 
 async function SWW(){
   const planets = ["Hoth", "Naboo", "Tatooine"]
-  while (true){
-    let senseData = await fetch('/sense-data');
-    senseData = await senseData.json();
+  let senseData = await fetch('/sense-data');
+  senseData = await senseData.json();
 
-    if (senseData.temp <= 10){
-      changeBG(planets[0], senseData.temp);
-    }
-    if (senseData.temp > 10){
-      changeBG(planets[1], senseData.temp);
-    }
-    if (senseData.temp > 25){
-      changeBG(planets[2], senseData.temp);
-    }
+  if (senseData.temp <= 10){
+    changeBG(planets[0], senseData.temp);
+  }
+  if (senseData.temp > 10){
+    changeBG(planets[1], senseData.temp);
+  }
+  if (senseData.temp > 25){
+    changeBG(planets[2], senseData.temp);
   }
 
 
