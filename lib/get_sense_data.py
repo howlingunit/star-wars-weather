e1051c2f77c0cf72cb894d86c2dce84b093098a3
howlingunit <93931085+howlingunit@users.noreply.github.com>
2022-11-06 18:12:26 +0000

    changed to test data and added new query

--- lib/get_sense_data.py
+++ lib/get_sense_data.py
@@ -2,24 +2,24 @@ import sqlite3
 import time
 import uuid
 
-# import random
+import random
 
-from sense_hat import SenseHat
-sense = SenseHat()
+# from sense_hat import SenseHat
+# sense = SenseHat()
 
 def getinfo():
 
     conn = sqlite3.connect('./database/sense_data.sqlite')
     c = conn.cursor()
 
-    P = sense.get_pressure()
-    T = sense.get_temperature() - 10 
-    H = sense.get_humidity()
+    # P = sense.get_pressure()
+    # T = sense.get_temperature() - 10 
+    # H = sense.get_humidity()
 
     # test data
-    # P = random.uniform(0.2, 30)
-    # T = random.uniform(0.2, 30)
-    # H = random.uniform(0.2, 30)
+    P = random.uniform(0.2, 30)
+    T = random.uniform(0.2, 30)
+    H = random.uniform(0.2, 30)
 
 
     c.execute("INSERT INTO data(id, date, temp, humid, press) VALUES ('{}', '{}', '{}', '{}', '{}')".format(str(uuid.uuid4()), time.time_ns(), T, H, P))
