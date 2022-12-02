d6afb8f3bd659e4a1cbebfc75cba3975433c2200
howlingunit <tyler_pitt@outlook.com>
2022-12-02 20:39:49 +0000

    average temp per month graph in test.html

--- static/lib/test.js
+++ static/lib/test.js
@@ -1,24 +1,43 @@
 const out = document.querySelector('#avg-temp');
 
-let data = await fetch('/week-avg');
+let data = await fetch('/months-avg');
 data = await data.json();
 
 const ctx = document.getElementById('myChart');
 
+const organisedData = []
+
+const months = {
+  '01': 'January',
+  '02': 'Febuary',
+  '03': 'March',
+  '04': 'April',
+  '05': 'May',
+  '06': 'June',
+  '07': 'July',
+  '08': 'August',
+  '09': 'September',
+  '10': 'October',
+  '11': 'November',
+  '12': 'December',
+}
+
+Object.keys(data).sort().forEach((month) => {
+  const dispMonth = months[month];
+
+  organisedData.push({
+    x: dispMonth,
+    y: data[month],
+  })
+})
+
+
 new Chart(ctx, {
   type: 'bar',
   data: {
-    // labels: Object.keys(data),
     datasets: [{
       label: 'average temps',
-      data: data,
+      data: organisedData,
     }]
   },
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true
-      }
-    }
-  }
 });
